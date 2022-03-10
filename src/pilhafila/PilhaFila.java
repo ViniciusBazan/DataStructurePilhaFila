149b3740585108406cf6aecf738dbddbc48f1fc0
ViniciusBazan <vinicius.bazan16@gmail.com>
2022-03-10 11:28:24 -0300

    Mudança dos tipos de dados e criação da fila, proposta concluída

--- src/pilhafila/PilhaFila.java
+++ src/pilhafila/PilhaFila.java
@@ -4,8 +4,8 @@
  */
 package pilhafila;
 
-import java.util.LinkedList;
-import java.util.Stack;
+import com.sun.jmx.remote.internal.ArrayQueue;
+import java.util.*;
 
 /**
  *
@@ -18,49 +18,95 @@ public class PilhaFila {
      */
     public static void main(String[] args) {
         
-    // Criando nossos objetos de Pilha e Fila
+    // Criando nossos objetos de Lista, Pilha e Fila
 
-    LinkedList<Integer> lista = new LinkedList<Integer>();
-    Stack<Integer> pilha = new Stack<Integer>();
+        ArrayList<Integer> lista = new ArrayList<>(5);
+        ArrayDeque<Integer> pilha = new ArrayDeque<>(5);
+        ArrayQueue<Integer> fila = new ArrayQueue<>(10);
 
-    // Inserindo valores iniciais na nossa lista
-    lista.add(1);
-    lista.add(2);
-    lista.add(3);
-    lista.add(4);
-    lista.add(5);
+        System.out.println(">> Objetos Vazios: ");
+        System.out.println("Lista[Capacidade - 5e]: "+lista);
+        System.out.println("Pilha[Capacidade - 5e]: "+pilha);
+        System.out.println("Fila[Capacidade - 10e]: "+fila);
+        System.out.println("");
+
+        // Inserindo valores iniciais na nossa lista
+        lista.add(1);
+        lista.add(2);
+        lista.add(3);
+        lista.add(4);
+        lista.add(5);
+
+        System.out.println(">> Inserindo 5 nº na lista");
+        System.out.println("Lista[Capacidade - 5e]: "+lista);
+        System.out.println("Pilha[Capacidade - 5e]: "+pilha);
+        System.out.println("Fila[Capacidade - 10e]: "+fila);
+        System.out.println("");
 
 
-    // Listando nossa lista do começo até o fim
-    System.out.println(">> Nossa lista tem os elementos: ");
-    for(int i = 0; i < lista.size(); i++){
-      System.out.println("e"+i+": "+lista.get(i));
-}
+        // Lista para Pilha
+        while(!lista.isEmpty()){
+            pilha.push(lista.get(0));
+            lista.remove(0);
+        }
 
-    System.out.println("");
+        System.out.println(">> Incrementando a Pilha e esvaziando a Lista");
+        System.out.println("Lista[Capacidade - 5e]: "+lista);
+        System.out.println("Pilha[Capacidade - 5e]: "+pilha);
+        System.out.println("Fila[Capacidade - 10e]: "+fila);
+        System.out.println("");
 
-    // Entrando com itens da lista na pilha
-    while(!lista.isEmpty()){
-      pilha.push(lista.getFirst());
-      lista.removeFirst();
-      }
 
-    // Voltando os elementos da pilha na fila e adicionando mais 5 números
-    while(!pilha.isEmpty()){
-      lista.add(pilha.pop());
-    }
+        // Pilha para fila
+        while(!pilha.isEmpty()){
+            fila.add(pilha.pop());
+        }
 
-    lista.add(6);
-    lista.add(7);
-    lista.add(8);
-    lista.add(9);
-    lista.add(10);
+        System.out.println(">> Incrementando a Fila e esvaziando a Pilha");
+        System.out.println("Lista[Capacidade - 5e]: "+lista);
+        System.out.println("Pilha[Capacidade - 5e]: "+pilha);
+        System.out.println("Fila[Capacidade - 10e]: "+fila);
+        System.out.println("");
 
-    System.out.println(">> Nossa lista atual tem os elementos: ");
-    for(int i = 0; i < lista.size(); i++){
-      System.out.println("e"+i+": "+lista.get(i));
-    }
-       
+
+        // Inserindo mais dados na Lista
+        lista.add(6);
+        lista.add(7);
+        lista.add(8);
+        lista.add(9);
+        lista.add(10);
+
+        System.out.println(">> Inserindo dados na Lista");
+        System.out.println("Lista[Capacidade - 5e]: "+lista);
+        System.out.println("Pilha[Capacidade - 5e]: "+pilha);
+        System.out.println("Fila[Capacidade - 10e]: "+fila);
+        System.out.println("");
+
+
+        // Lista para Pilha
+        while(!lista.isEmpty()){
+            pilha.push(lista.get(0));
+            lista.remove(0);
+        }
+
+        System.out.println(">> Incrementando a Pilha e esvaziando a Lista");
+        System.out.println("Lista[Capacidade - 5e]: "+lista);
+        System.out.println("Pilha[Capacidade - 5e]: "+pilha);
+        System.out.println("Fila[Capacidade - 10e]: "+fila);
+        System.out.println("");
+
+
+        // Pilha para fila
+        while(!pilha.isEmpty()){
+            fila.add(pilha.pop());
+        }
+
+        System.out.println(">> Incrementando a Fila e esvaziando a Pilha");
+        System.out.println("Lista[Capacidade - 5e]: "+lista);
+        System.out.println("Pilha[Capacidade - 5e]: "+pilha);
+        System.out.println("Fila[Capacidade - 10e]: "+fila);
+        System.out.println("");
+    
     }
     
 }
